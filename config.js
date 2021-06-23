0775e5c879705aa0fa9de09f8faffcc00903b19c
thong1620 <86162542+thong1620@users.noreply.github.com>
2021-06-23 15:26:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Mớ ớii!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,14 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Hong yêu thong xíu nàoo',
-    btnNo: 'Yêu thong <3',
-    question:'đến đây là biết chọn khong yêu ròi :(',
-    btnReply: 'Say something - -',
-    reply: 'Chọn khong yêu nma yêu thì yêu không yêu thì yêu <33333333',
-    mess: 'Thong biết mà 🥰. Yêu mớ nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h anh qua đón nhaa.',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 9h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
+
